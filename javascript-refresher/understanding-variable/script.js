25dab41220f06404a09c967ab936071bd6032661
TedEngineer <tedvu184@gmail.com>
2023-01-31 23:35:31 +1100

    Add var concept

--- javascript-refresher/understanding-variable/script.js
+++ javascript-refresher/understanding-variable/script.js
@@ -1,3 +1,11 @@
+"use strict";
+/*
+var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value
+*/
+
+// example 1: simple variable declaration
 var greeter = "hello world";
 
 console.log(greeter);
+
+// example 2:
